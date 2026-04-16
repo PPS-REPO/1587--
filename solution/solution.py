7f8e74513b26e732ecf847c067863ad4652a9568
pps-admin <pps.github@gmail.com>
2026-04-16 12:20:59 +0900

    Update: path 'solution/solution.py' - Updated by 'misemise'

--- solution/solution.py
+++ solution/solution.py
@@ -1,1 +1,11 @@
-print(sum(map(int,input().split())))
+import heapq
+n,m=map(int,input().split())
+l=list(map(int,input().split()))
+h=[]
+nlw=0
+for i in l:
+    if len(h)<m:heapq.heappush(h,i)
+    else:
+        heapq.heappush(h,i)
+        if heapq.heappop(h)==i:nlw+=1
+print(nlw)
