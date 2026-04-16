a6ab70d21d400d5d65078df7e50da74daf01a013
pps-admin <pps.github@gmail.com>
2026-04-16 12:28:19 +0900

    Update: path 'solution/solution_wa.cc' - Updated by 'misemise'

--- solution/solution_wa.cc
+++ solution/solution_wa.cc
@@ -1,8 +1,23 @@
-#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 
 int main() {
-    int a, b;
-    scanf("%d%d", &a, &b);
-    printf("%d\n", a + b + 1);
+    int n, m;
+    std::cin >> n >> m;
+    std::vector<int> l(n);
+    for (int i = 0; i < n; ++i) {
+        std::cin >> l[i];
+    }
+    std::vector<int> pc;
+    int nlw = 0;
+    for (int i : l) {
+        pc.push_back(i);
+        std::sort(pc.begin(), pc.end());
+        auto it = std::find(pc.begin(), pc.end(), i);
+        int j = std::distance(pc.begin(), it);
+        if (j >= m) nlw++;
+    }
+    std::cout << nlw << std::endl;
     return 0;
 }
