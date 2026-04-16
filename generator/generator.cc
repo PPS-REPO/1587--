da20d5ae8d4d8ff2d780d528bb6061a32c353e02
pps-admin <pps.github@gmail.com>
2026-04-16 12:55:44 +0900

    Update: path 'generator/generator.cc' - Updated by 'misemise'

--- generator/generator.cc
+++ generator/generator.cc
@@ -1,20 +1,41 @@
 #include "testlib.h"
 
+#include <iostream>
+#include <vector>
+#include <numeric>
+#include <algorithm>
+#include <random>
+
 using namespace std;
 
-int main(int argc, char** argv) {
-    int al, ar, bl, br, a, b;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    random_device rd;
+    mt19937 gen(rd());
+
+    uniform_int_distribution<int> dist_n(1, 300000);
+    int n = dist_n(gen);
+
+    uniform_int_distribution<int> dist_m(1, n);
+    int m = dist_m(gen);
+
+    cout << n << " " << m << "\n";
+
     
-    registerGen(argc, argv, 1);
+    uniform_int_distribution<int> dist_val(1, 1000000000);
+    vector<int> l;
+    l.reserve(n);
+    for(int i = 0; i < n; ++i) {
+        l.push_back(dist_val(gen));
+    }
 
-    al = atoi(argv[1]);
-    ar = atoi(argv[2]);
-    bl = atoi(argv[3]);
-    br = atoi(argv[4]);
+    // 3. 결과 출력
+    for (int i = 0; i < n; ++i) {
+        cout << l[i] << (i == n - 1 ? "" : " ");
+    }
+    cout << endl;
 
-    a = rnd.next(al, ar);
-    b = rnd.next(bl, br);
-
-    printf("%d %d\n", a, b);
     return 0;
 }
